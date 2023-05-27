d0762fc6c7c55217b99222ad4a39c1a9736619df
AlexanderZon <amontenegro.sistemas@gmail.com>
2023-05-27 17:13:16 -0300

    Mejora el slider option para que visualice la opción seleccionada

--- lib/widgets/slider_options_picker.dart
+++ lib/widgets/slider_options_picker.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SliderOptionsPicker extends StatefulWidget {
@@ -22,20 +24,27 @@ class SliderOptionsPicker extends StatefulWidget {
   State<SliderOptionsPicker> createState() => _SliderOptionsPickerState();
 }
 
-class _SliderOptionsPickerState extends State<SliderOptionsPicker> {
-
-  final ScrollController scrollController = ScrollController();
-  
-  @override
-  void initState() {
-    super.initState();
-    scrollController.addListener(() {
-      //
-    });
-  }
+class _SliderOptionsPickerState extends State<SliderOptionsPicker> {  
 
   @override
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
+    double optionHeight = 50;
+    double optionWidth = 50;
+    double sideSlackWidth = width/2-(optionWidth/2);
+    
+    var index = widget.options.indexWhere((element) => element == widget.formValues[widget.formProperty]); 
+    final ScrollController scrollController = ScrollController(initialScrollOffset: index*optionWidth);
+  
+    @override
+    void initState() {
+      super.initState();
+      scrollController.addListener(() {
+        //
+      });
+    }
+
     return SizedBox(
       width: double.infinity,
       child: Column(
@@ -45,7 +54,7 @@ class _SliderOptionsPickerState extends State<SliderOptionsPicker> {
             margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
             child: Text(
               widget.label,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 16, fontFamily: "Genshin"),
             ),
           ),
           Container(
@@ -53,38 +62,77 @@ class _SliderOptionsPickerState extends State<SliderOptionsPicker> {
             width: double.infinity,
             height: 50,
             // decoration: BoxDecoration(border: Border.symmetric(horizontal: BorderSide(color: Colors.indigo.withAlpha(128)))),
-            child: NotificationListener(
-              onNotification: (t) {
-                 if (t is ScrollEndNotification) {
-                  // log('End Notification: ${scrollController.position.pixels} ${scrollController.position.maxScrollExtent}');
-                }
-                return false;
-              },
-              child: ListView.builder(
-                itemCount: widget.options.length,
-                scrollDirection: Axis.horizontal,
-                controller: scrollController,
-                itemBuilder: (context, int index){
-                  if(widget.options[index] == widget.formValues[widget.formProperty]){
-                    return TextButton(
-                      onPressed: () {}, 
-                      style: const ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll<Color>(Colors.indigo),
+            child: Stack(
+              children: [
+                Container(
+                  height: double.infinity,
+                  child: Row(
+                    children: [
+                      SizedBox(width: sideSlackWidth, height: optionHeight,),
+                      SizedBox(
+                        width: optionWidth, 
+                        height: optionHeight, 
+                        child: Container( 
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5), 
+                            border: Border.all(color: Colors.indigo, width: 2),
+                            color: Colors.indigo.withOpacity(.25),
+                          ),
+                        ),
                       ),
-                      child: Text('${widget.options[index]}', style: const TextStyle(color: Colors.white),)
-                    );
-                  }
-                  return TextButton(
-                    onPressed: () {
+                    ]
+                  ),
+                ),
+                NotificationListener(
+                  onNotification: (t) {
+                     if (t is ScrollEndNotification) {
+                      double actualValue = (scrollController.position.pixels)/optionWidth;
+                      var nextIntValue = actualValue.ceil();
+                      var previousIntValue = actualValue.floor();
+                      int nextOption = 0;
+                      if((nextIntValue-actualValue) < (actualValue-previousIntValue)){
+                        nextOption = nextIntValue;
+                      } else {
+                        nextOption = previousIntValue;
+                      }
+                      // Set selected option on state
                       setState(() {
-                        widget.formValues[widget.formProperty] = widget.options[index];
+                        widget.formValues[widget.formProperty] = widget.options[nextOption];
                         widget.onChange();
+
+                        log("${widget.formProperty}: ${widget.formValues[widget.formProperty]}");
                       });
-                    }, 
-                    child: Text('${widget.options[index]}', style: const TextStyle(color: Colors.indigo),)
-                  );
-                },
-              ),
+                      // Set selected option visible
+                      try {
+                        scrollController.position.setPixels(nextOption*optionWidth);
+                      } catch (_AssertionError){
+                        log("Error");
+                      }
+                    }
+                    return false;
+                  },
+                  child: ListView.builder(
+                    itemCount: widget.options.length+2,
+                    scrollDirection: Axis.horizontal,
+                    controller: scrollController,
+                    itemBuilder: (context, int index){
+                      if(index == 0) return SizedBox(width: sideSlackWidth,);
+                      if(index == widget.options.length+1) return SizedBox(width: sideSlackWidth,);
+                      int optionIndex = index - 1;
+                      return SizedBox(
+                        width: optionWidth,
+                        height: optionWidth,
+                        child: Center(
+                          child: Text(
+                            '${widget.options[optionIndex]}', 
+                            style: const TextStyle(color: Colors.indigo, fontFamily: "Genshin", fontSize: 14),
+                          )
+                        )
+                      );
+                    },
+                  ),
+                ),
+              ],
             )
           )
         ],
@@ -92,3 +140,74 @@ class _SliderOptionsPickerState extends State<SliderOptionsPicker> {
     );
   }
 }
+
+// class _SliderOptionsPickerState extends State<SliderOptionsPicker> {
+
+//   final ScrollController scrollController = ScrollController();
+  
+//   @override
+//   void initState() {
+//     super.initState();
+//     scrollController.addListener(() {
+//       //
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: double.infinity,
+//       child: Column(
+//         children: [
+//           Container(
+//             alignment: Alignment.centerLeft,
+//             margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+//             child: Text(
+//               widget.label,
+//               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//           Container(
+//             margin: const EdgeInsets.only(bottom: 10),
+//             width: double.infinity,
+//             height: 50,
+//             // decoration: BoxDecoration(border: Border.symmetric(horizontal: BorderSide(color: Colors.indigo.withAlpha(128)))),
+//             child: NotificationListener(
+//               onNotification: (t) {
+//                  if (t is ScrollEndNotification) {
+//                   // log('End Notification: ${scrollController.position.pixels} ${scrollController.position.maxScrollExtent}');
+//                 }
+//                 return false;
+//               },
+//               child: ListView.builder(
+//                 itemCount: widget.options.length,
+//                 scrollDirection: Axis.horizontal,
+//                 controller: scrollController,
+//                 itemBuilder: (context, int index){
+//                   if(widget.options[index] == widget.formValues[widget.formProperty]){
+//                     return TextButton(
+//                       onPressed: () {}, 
+//                       style: const ButtonStyle(
+//                         backgroundColor: MaterialStatePropertyAll<Color>(Colors.indigo),
+//                       ),
+//                       child: Text('${widget.options[index]}', style: const TextStyle(color: Colors.white),)
+//                     );
+//                   }
+//                   return TextButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         widget.formValues[widget.formProperty] = widget.options[index];
+//                         widget.onChange();
+//                       });
+//                     }, 
+//                     child: Text('${widget.options[index]}', style: const TextStyle(color: Colors.indigo),)
+//                   );
+//                 },
+//               ),
+//             )
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
